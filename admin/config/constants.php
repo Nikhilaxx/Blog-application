6011f5faee8994674aff6cec9db670375cad1196
Nikhilaxx <nikhish07@gmail.com>
2025-02-27 23:50:23 +0530

    Initial and final commit: Added PHP Blog Application with core files and structure

---
+++ admin/config/constants.php
@@ -0,0 +1,10 @@
+
+<?php
+
+session_start(); //Because session is used
+
+define('ROOT_URL', 'http://localhost/blog/');
+define('DB_HOST', 'localhost:3307');
+define('DB_USER','blog2');
+define('DB_PASS','admin123');
+define('DB_NAME','blog2');
---
+++ admin/partials/header.php
@@ -0,0 +1,10 @@
+<?php
+require '../partials/header.php';
+
+
+//CHECK LOGIN STATUS 
+if(!isset($_SESSION['user-id'])) {
+    header('location: ' . ROOT_URL . 'signin.php');
+    die();
+}
+?>
